68f7fed6528075dfe0a19452fc2e6cdf83ffb7a9
Avinash Reddy <avinashreddy3108+github@gmail.com>
2023-11-16 11:06:56 +0530

    fix: explicity specify content type and send data as json (#56) (#2)
    * fix: explicity specify content type and send data as json
    
    * chore: duplicate setting of headers
    
    ---------
    
    Co-authored-by: Dany Robert <rtdany10@gmail.com>
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>

--- erpnext_sync.py
+++ erpnext_sync.py
@@ -373,7 +373,7 @@ def send_to_erpnext(employee_field_value, timestamp, device_id=None, log_type=No
         "device_id": device_id,
         "log_type": log_type,
     }
-    response = requests.request("POST", url, headers=headers, data=data)
+    response = requests.request("POST", url, headers=headers, json=data)
     if response.status_code == 200:
         return 200, json.loads(response._content)["message"]["name"]
     error_str = _safe_get_error_str(response)
