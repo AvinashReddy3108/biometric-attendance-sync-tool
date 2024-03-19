f3eb1b2f854df0557079910ca5d78c5a7eabbca6
Avinash Reddy <avinashreddy3108+github@gmail.com>
2024-03-19 20:15:06 +0530

    linux script: generate systemd unit file if not exists

--- run.sh
+++ run.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Move to main directory
 cd "$(dirname "$0")" || exit
 
 # Activate virtual environment
 [ ! -d "$PWD/.venv" ] && python3 -m venv .venv
-source $PWD/.venv/bin/activate
+source "$PWD/.venv/bin/activate"
 
 # Install requirements
 pip install -r requirements.txt
+
+# Creating the systemd service file for later use
+[ ! -f "$PWD/biometric_attendance_sync.service" ] && cat <<- EOF > "$PWD/biometric_attendance_sync.service"
+[Unit]
+Description=ERPNext Biometric Attendance Sync
+Wants=network-online.target network.target
+After=network.target
+
+[Service]
+WorkingDirectory=$PWD
+ExecStart=$PWD/.venv/bin/python erpnext_sync.py
+Restart=always
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # Run
 exec python erpnext_sync.py
